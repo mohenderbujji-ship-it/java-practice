c1c1ac3d7fb042fdcc3c6df789620bcaf8002631
mohenderbujji-ship-i <mohenderbujji@gmail.com>
2026-01-29 12:36:13 +0530

    add: added practice codes

---
+++ practice.java
@@ -0,0 +1,27 @@
+class Neymar{
+public static void main(String args[]){
+// entering the own details
+System.out.println("Enter your details:");
+String name="Mohender";
+byte age=21;
+long phone_no= 7806867041l;
+char bloodGroup='A';
+String address= "chennai";
+System.out.println(" Name: "+ name);
+System.out.println(" Age: "+ age);
+System.out.println(" Phone no: "+ phone_no);
+System.out.println(" BloodGroup: "+ bloodGroup);
+System.out.println("Address "+ address);
+System.out.println();
+// now entering the parent details
+System.out.println("Enter parent details:");
+String father_name="senthil kumar";
+String mother_name="julie samathanam";
+int monthly_income= 50000;
+long gaurdian_no= 9626388839l;
+System.out.println(" Father Name: "+ father_name);
+System.out.println(" Mother Name: "+ mother_name);
+System.out.println(" Monthly Salary: "+ monthly_income);
+System.out.println(" Gaurdian Number: "+ gaurdian_no);
+}
+}
